a195ad1d25fd1e772ee00a597b7ea973aa06749f
Youssef548 <youssefahmedpvp@gmail.com>
2022-09-29 17:08:08 +0200

    :heavy_check_mark:

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,20 +1,111 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
+  const initValue = { username: "", email: "", message: "" };
+  const form = useRef();
+  const [isSubmited, setIsSubmited] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formValues, setFormValues] = useState(initValue);
+
+  // const [isValid, setIsValid] = useState();
+  // const [hasError, setHasError] = useState();
+  // setFromErrors(validate(formValues));
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .sendForm(
+        "service_htgmbuw",
+        "template_8q7i7xg",
+        form.current,
+        "hTzHqrfXxBfu1b0M1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsSubmited(true);
+      setFormValues(initValue);
+    }, 2000);
+  };
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+  // const validate = (values) => {
+  //   const errors = {};
+  //   const regex =
+  //     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  //   if (!values.name) {
+  //     errors.name = "name is required!";
+  //   }
+  //   if (!values.email) {
+  //     errors.email = "email is required!";
+  //   } else if (!regex.test(values.email)) {
+  //     errors.email = "this is email invalid!";
+  //   }
+  //   if (!values.message) {
+  //     errors.message = "message is required!";
+  //   }
+  //   return errors;
+  // };
+
+  // if (Object.keys(formErrors).length === 0 && isSubmit) {
+  // }
+
   return (
     <>
       <h3 className="form-title">Contact me, let’s make magic together</h3>
-      <form className="form" onSubmit={() => console.log("clicked")}>
-        <input type="text" className="name" placeholder="Name:" />
-        <input type="email" className="email" placeholder="Email:" />
+      <form ref={form} className="form" onSubmit={sendEmail}>
+        <input
+          type="text"
+          className="name"
+          placeholder="Name:"
+          name="name"
+          value={formValues.name}
+          onChange={changeHandler}
+          required
+        />
+
+        <input
+          type="email"
+          className="email"
+          placeholder="Email:"
+          name="email"
+          value={formValues.email}
+          onChange={changeHandler}
+          // required
+        />
+
         <textarea
-          name=""
           id=""
           cols="30"
           rows="10"
           placeholder="Message"
+          name="message"
+          value={formValues.message}
+          onChange={changeHandler}
+          required
         ></textarea>
-        <button className="btn btn-form">Send</button>
+        {isSubmited ? (
+          <p className="success-message-contact">
+            Your message has been successfully sent.
+          </p>
+        ) : (
+          <button className="btn btn-form">
+            {isLoading ? "Sending.." : "Send"}
+          </button>
+        )}
       </form>
     </>
   );
